8c1b208d1ccbdcb71d7ac6db62365f943ec1722c
sahilkhan09k <sahilkhan392005@gmail.com>
2025-04-28 20:43:35 +0530

    Project finalized

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,6 +3,7 @@ const PokemonCard = ({ pokemon }) => {
       <div className="bg-white cursor-pointer rounded-xl shadow-md p-4 flex flex-col items-center hover:scale-105 transition-transform ">
         <img src={pokemon.image} alt={pokemon.name} className="w-50 h-50" />
         <h2 className="mt-2 font-bold capitalize text-[30px]">{pokemon.name}</h2>
+        <p className="text-gray-500 text-[18px]">ID: {pokemon.id}</p>
         <div className="flex gap-2 mt-4">
           {pokemon.types.map((type) => (
             <span
